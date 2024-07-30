b1ed5352773fa6dbaf12a72abd01be8c34f72bd7
Ecem <ecemnazgorusuk@gmail.com>
2024-07-30 16:12:08 +0300

    Login,getUserInfo & userCreate tests are done.

--- tests/userTests.js
+++ tests/userTests.js
@@ -1,6 +1,8 @@
 //chakram kütüphanesi import edilir
 const chakram = require('chakram'),
   expect = chakram.expect;
+
+
 /*
   describe -> bir grup testin bir arada olduğu bloğu temsil eder
   it -> blokta bulunan testlerin her biridir
@@ -9,19 +11,58 @@ const chakram = require('chakram'),
   kullanarak API çağrıları yapabiliriz 
   Örnek : const response = chakram.get("https://petstore.swagger.io/v2/user/");
 */
+
 describe("User Test", function () {
+
+  //GET USER INFO TEST
   it("getUserInfo", function () {
-    const response = chakram.get("https://petstore.swagger.io/v2/user/ecem");
+    const response = chakram.get("https://petstore.swagger.io/v2/user/berk");
     //gelen status code doğruluğunu teyit etmek
     expect(response).to.have.status(200); //ok
     return chakram.wait();
   });
+
+  //USER CREATE TEST
+  it("userCreate", function () {
+    const requestBody={
+      "id": 1,
+      "username": "berk",
+      "firstName": "Berk",
+      "lastName": "Akkavak",
+      "email": "berkak@gmail.com",
+      "password": "12345",
+      "phone": "1231454534",
+      "userStatus": 0
+    }
+    const response = chakram.post("https://petstore.swagger.io/v2/user",requestBody);
+    //gelen status code doğruluğunu teyit etmek
+    expect(response).to.have.status(200); //ok
+    return chakram.wait();
+  });
+
+
+  //GET WRONG USER TEST
   it("getWrongUserInfo", function () {
     const response = chakram.get("https://petstore.swagger.io/v2/user/ecem1");
     //gelen status code doğruluğunu teyit etmek
     expect(response).to.have.status(404); //not found
     return chakram.wait();
   });
+
+
+  //LOGIN TESTİ
+  it("login", function () {
+    const qsBody={
+      "username":"berk",
+      "password":"12345"
+    }
+    const response = chakram.get("https://petstore.swagger.io/v2/user/login", {qs:qsBody});
+    //gelen status code doğruluğunu teyit etmek
+    expect(response).to.have.status(200); //ok
+    return chakram.wait();
+  });
+
+  
 });
 
 
