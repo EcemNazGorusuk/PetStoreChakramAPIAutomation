ac71de14283e7513b0d2069efbfd6eb886da1df4
Ecem <ecemnazgorusuk@gmail.com>
2024-07-30 23:00:08 +0300

    deletePet,getPet and createPet tests are done.

--- tests/petTests.js
+++ tests/petTests.js
@@ -3,26 +3,27 @@ const chakram = require("chakram"),
   expect = chakram.expect;
 
 describe("Pet Test", function () {
+
   //PET CREATE TEST
   it("/v2/pet createPet", function () {
     const requestBody = {
-        "id": 9876543,
-        "category": {
-          "id": 477473,
-          "name": "pet"
+      id: 9876543,
+      category: {
+        id: 477473,
+        name: "pet",
+      },
+      name: "Boncuk",
+      photoUrls: [
+        "https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/activities-fun/10-great-small-dog-breeds/maltese-portrait.jpg",
+      ],
+      tags: [
+        {
+          id: 1,
+          name: "cat",
         },
-        "name": "Boncuk",
-        "photoUrls": [
-          "https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/activities-fun/10-great-small-dog-breeds/maltese-portrait.jpg"
-        ],
-        "tags": [
-          {
-            "id": 1,
-            "name": "cat"
-          }
-        ],
-        "status": "available"
-      };
+      ],
+      status: "available",
+    };
     const response = chakram.post(
       "https://petstore.swagger.io/v2/pet",
       requestBody
@@ -31,6 +32,41 @@ describe("Pet Test", function () {
     return chakram.wait();
   });
 
- 
-});
 
+  
+   //GET PET TEST
+   it("/v2/pet getPet", function () {
+    // const requestBody = {
+    //   id: 9876543,
+    //   category: {
+    //     id: 477473,
+    //     name: "pet",
+    //   },
+    //   name: "Boncuk",
+    //   photoUrls: [
+    //     "https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/activities-fun/10-great-small-dog-breeds/maltese-portrait.jpg",
+    //   ],
+    //   tags: [
+    //     {
+    //       id: 1,
+    //       name: "cat",
+    //     },
+    //   ],
+    //   status: "available",
+    // };
+
+    // const response = chakram.post("https://petstore.swagger.io/v2/pet",requestBody); //hata almamak için
+    // expect(response).to.have.status(200);
+    const response2 = chakram.get("https://petstore.swagger.io/v2/pet/9876543");
+    expect(response2).to.have.status(200); //ok
+    return chakram.wait();
+  });
+
+ 
+  //DELETE TEST
+  it("/v2/pet deletePet", function () {
+    const response = chakram.delete(" https://petstore.swagger.io/v2/pet/9876543");
+    expect(response).to.have.status(200); //ok
+    return chakram.wait();
+  });
+});
